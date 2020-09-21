f9e2fa4bec34eb1c1363175402bf79c2ee71efa7
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-09-20 19:49:01 -1000

    Finished exercise

---
+++ sumofnumbers.js
@@ -0,0 +1,39 @@
+const nums = [1, 2, 3, 4, 5];
+
+function sumFor(num) {
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += num[i];
+  }
+  return sum;
+}
+
+console.log(sumFor(nums));
+
+function sumWhile(num) {
+  let sum = 0;
+  let i = 0;
+  while (i < num.length) {
+    sum += num[i];
+    i++;
+  }
+  return sum;
+}
+
+console.log(sumWhile(nums));
+
+
+function sumRecursion(num) {
+  if (num.length === 0) {
+    return 0;
+  }
+  return num[0] + sumRecursion(num.slice(1, num.length));
+}
+
+console.log(sumRecursion(nums));
+
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(nums));
